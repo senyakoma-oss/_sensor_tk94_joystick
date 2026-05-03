6f29738f2ce03bfae1d6e81474e992a78d1d775e
senyakoma-oss <senyakoma@gmail.com>
2026-05-03 13:28:08 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
-let joystickStateResult = ""
-function joystickState (sensor: number) {
-    joystickStateResult = "Steady"
-    if (sensor > 50 && sensor < 150) {
-        joystickStateResult = "Up"
-    } else if (sensor > 1000) {
-        joystickStateResult = "Right"
-    } else if (sensor > 450 && sensor < 550) {
-        joystickStateResult = "Down"
-    } else if (sensor > 250 && sensor < 350) {
-        joystickStateResult = "Left"
-    } else if (sensor > 150 && sensor < 250) {
-        joystickStateResult = "Pressed"
+namespace joystick {
+    /**
+     * Определяет состояние джойстика
+     */
+    //% block="joystick state %sensor"
+    //% weight=100 color=#0fbc11 icon="\uf11b"
+    //% sensor.min=0 sensor.max=1023
+    export function joystickState(sensor: number): string {
+        let joystickStateResult = "Steady"
+
+        if (sensor > 50 && sensor < 150) {
+            joystickStateResult = "Up"
+        } else if (sensor > 1000) {
+            joystickStateResult = "Right"
+        } else if (sensor > 450 && sensor < 550) {
+            joystickStateResult = "Down"
+        } else if (sensor > 250 && sensor < 350) {
+            joystickStateResult = "Left"
+        } else if (sensor > 150 && sensor < 250) {
+            joystickStateResult = "Pressed"
+        }
+
+        return joystickStateResult
     }
-    return joystickStateResult
 }
